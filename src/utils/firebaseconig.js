79794b8156b1593d47111cd6a390c28c53e28573
supunnilakshana <supunnilakshana1999@gmail.com>
2022-02-23 16:42:50 +0530

    nav-bar1

---
+++ src/utils/firebaseconig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { FirebaseApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAPHCBbieerIh4KAKVTReakZxx3elS_dHE",
+  authDomain: "blogapp-c39cd.firebaseapp.com",
+  projectId: "blogapp-c39cd",
+  storageBucket: "blogapp-c39cd.appspot.com",
+  messagingSenderId: "495100505159",
+  appId: "1:495100505159:web:2e6d18e31b94688bdc5562",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
