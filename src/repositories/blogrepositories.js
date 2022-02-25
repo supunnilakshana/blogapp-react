58b20c9db0e7eee761cdf0f5e810c120698d2871
supunnilakshana <supunnilakshana1999@gmail.com>
2022-02-25 22:11:51 +0530

    uniqe id

--- src/repositories/blogrepositories.js
+++ src/repositories/blogrepositories.js
@@ -1,12 +1,14 @@
 import { set,ref, } from "firebase/database";
 import { db } from "../utils/firebaseconig";
+import { v4 as uuidv4 } from 'uuid';
 
 function addpost(postobj){
   var currentdate = new Date();
   console.log(currentdate);
+  const pid=uuidv4();
     return new Promise(async (resolve, reject) => {
        
-        await set(ref(db, `post/${1}`), postobj)
+        await set(ref(db, `post/${pid}`), postobj)
           .then( () => {
          
             resolve("message_added");
